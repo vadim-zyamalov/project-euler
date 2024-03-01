005b2e68004f35e288efe51d10d13a392aa7c764
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2024-03-01 12:56:26 +0300

    Косметические изменения

--- Page 01/problem0002/variant3.hs
+++ Page 01/problem0002/variant3.hs
@@ -2,4 +2,4 @@ fib :: [Int]
 fib = 1 : 1 : zipWith (+) fib (tail fib)
 
 main :: IO()
-main = print $ sum $ filter even $ takeWhile (< 4_000_000) fib
+main = print . sum . filter even . takeWhile (< 4_000_000) $ fib
