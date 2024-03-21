0cad75b0862033652a5c6f3560d00b8bff84ec2d
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2024-03-21 11:41:54 +0300

    Задача 44 без предела "от балды"

--- Problems 001-050/p44.py
+++ Problems 001-050/p44.py
@@ -10,13 +10,13 @@ def isPnum(p: int) -> bool:
 def solve():
     pnums = [pnum(i) for i in range(100_01)]
 
-    for n in range(1, 100_01):
+    n = 1
+    while True:
         for m in range(1, n):
             s, d = pnums[n] + pnums[m], pnums[n] - pnums[m]
             if isPnum(s) and isPnum(d):
                 return d
-
-    return -1
+        n += 1
 
 
 print(solve())
